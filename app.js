25642a5a0f404781882cb6c33855766b4ad4dc9f
ekeskn <ege-keskintas@hotmail.com>
2022-04-11 21:34:48 +0300

    template added

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: 'Blog title',
-        description: 'Blog description',
-    };
-    res.send(blog)
+    res.render('index')
+});
+app.get('add_post', (req, res) => {
+    res.render('add_post')
+});
+app.get('post', (req, res) => {
+    res.render('post')
+});
+app.get('about', (req, res) => {
+    res.render('about')
 });
 
 const port = 5000;
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} da başlatıldı..`)
-})
+    console.log(`Sunucu ${port} da başlatıldı..`);
+});
